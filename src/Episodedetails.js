d9f2753b4650520ec3ef018e618b7712da971faa
john <john310897@gmail.com>
2021-02-12 00:21:32 +0530

    episode details
    updated episodedetails page

--- src/Episodedetails.js
+++ src/Episodedetails.js
@@ -7,29 +7,43 @@ function Episodedetails({match}){
         console.log(match);
     },[]);
     const [items,setItems]=useState([]);
-
     const fetchData=async()=>{
         const data=await fetch(`https://www.breakingbadapi.com/api/episodes/${match.params.id}`);
         const episodeData=await data.json();
         console.log(episodeData);
-        setItems(episodeData);
+        setItems(episodeData);        
     }
     return(
+        
         <div className='container'>
-            <h1 align='center'>EPISODE DETAILS</h1>
-            <div className='card-columns'>
-            {
-                
-                items.map((item,i=item.characters.length)=>(
-                    <Link><div className='card bg-light text-center'>
-                    <br/><h2>Episode: {item.episode_id}</h2><br/>
-                    <h4 key={item.episode_id}>{item.characters}</h4>
-                    </div></Link>
+            <h1 align='center'>Characters List</h1>
+            {   
+                items.map((item)=>
+                (
+                    
+                    
+                        <div className='card-columns'>
+                            
+                                <h4 className='card bg-light text-center'>{item.characters[0]}</h4>
+                                <h4 className='card bg-light text-center'>{item.characters[1]}</h4>
+                                <h4 className='card bg-light text-center'>{item.characters[2]}</h4>
+                                <h4 className='card bg-light text-center'>{item.characters[3]}</h4>
+                                <h4 className='card bg-light text-center'>{item.characters[4]}</h4>
+                                <h4 className='card bg-light text-center'>{item.characters[5]}</h4>
+                                <h4 className='card bg-light text-center'>{item.characters[5]}</h4>
+                                <h4 className='card bg-light text-center'>{item.characters[6]}</h4>
+                                <h4 className='card bg-light text-center'>{item.characters[7]}</h4>
+                                <h4 className='card bg-light text-center'>{item.characters[8]}</h4>  
+                                <h4 className='card bg-light text-center'>{item.characters[9]}</h4>  
+                                <h4 className='card bg-light text-center'>{item.characters[10]}</h4>   
+                      </div>
+                    
+                    
                 ))
             }
-            </div>
+        
         </div>
     );
-
+    
 }
 export default Episodedetails;
